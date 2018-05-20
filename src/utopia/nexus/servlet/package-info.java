f32e29dffb7e4871716e188b2d941511c6dc38ae
Mikkomario <mikko_hilpinen@hotmail.com>
2018-05-20 19:41:54 +0300

    Initial commit. Added HttpExtensions from Test Server project.

---
+++ src/utopia/nexus/servlet/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains extended features targeted specifically for (tomcat) scala servlets
+ */
+/**
+ * @author Mikko Hilpinen
+ * @since 20.5.2018
+ */
+package utopia.nexus.servlet;
---
+++ src/utopia/nexus/test/servlet/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains test servlet implementations
+ */
+/**
+ * @author Mikko Hilpinen
+ * @since 20.5.2018
+ */
+package utopia.nexus.test.servlet;
